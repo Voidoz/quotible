4a8f0cf47ad513fb9f95157cafa31741443dcc87
Voidoz <41652531+Voidoz@users.noreply.github.com>
2020-11-06 23:09:15 +1300

    added Quote data model

---
+++ server/database/schemas/Quote.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const { MongooseAutoIncrementID } = require('mongoose-auto-increment-reworked');
+
+// We make the Schema here
+const { Schema } = mongoose;
+
+// Here is our data model
+const quoteSchema = new Schema({
+  franchise: { type: String, required: true },
+  installment: { type: String },
+  charachter: { type: String },
+  quote: { type: String, required: true },
+});
+
+// Here we tell Mongoose how we want to treat the schema
+MongooseAutoIncrementID.initialise('counters');
+
+quoteSchema.plugin(MongooseAutoIncrementID.plugin, {
+  modelName: 'Quote',
+  field: 'quote',
+  incrementBy: 1,
+  startAt: 1,
+  unique: true,
+  nextCount: false,
+  resetCount: false,
+});
+
+// We define the data model here
+const Quote = mongoose.model('Quote', quoteSchema);
+
+// And then we export it
+module.exports = Quote;
--- server/database/schemas/index.js
+++ server/database/schemas/index.js
@@ -1,9 +1,11 @@
 const Session = require('./Session');
 const User = require('./User');
 const Todo = require('./Todo');
+const Quote = require('./Quote');
 
 module.exports = {
   Session,
   User,
   Todo,
+  Quote,
 };
